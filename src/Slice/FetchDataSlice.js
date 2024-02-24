6b96258c1d3ed9f51d2bfeeba225c48735d48b16
Ashutosh-pixel <ashushar222@gmail.com>
2024-02-24 21:37:03 +0530

    api key hide

--- src/Slice/FetchDataSlice.js
+++ src/Slice/FetchDataSlice.js
@@ -1,12 +1,15 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import { env } from "../../env";
+// import "dotenv/config";
+
+// require("dotenv").config();
 
 export const fetchdata = createAsyncThunk("fetchdata", async () => {
-  const url =
-    "https://coinranking1.p.rapidapi.com/coins?referenceCurrencyUuid=yhjMzLPhuIDl&timePeriod=24h&tiers%5B0%5D=1&orderBy=marketCap&orderDirection=desc&limit=50&offset=0";
+  const url = env.baseurl;
   const options = {
     method: "GET",
     headers: {
-      "X-RapidAPI-Key": "6386f1e016msh80f6bf8041acd09p1331e3jsn7d0adafec67f",
+      "X-RapidAPI-Key": env.key,
       "X-RapidAPI-Host": "coinranking1.p.rapidapi.com",
     },
   };
