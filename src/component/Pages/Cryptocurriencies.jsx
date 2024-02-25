59813ea561e58255fa3e5e27ccefa531f4608236
Ashutosh-pixel <ashushar222@gmail.com>
2024-02-25 15:57:25 +0530

    search box cryptopage

--- src/component/Pages/Cryptocurriencies.jsx
+++ src/component/Pages/Cryptocurriencies.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import CryptoCard from "./CryptoCard";
 import { fetchdata } from "../../Slice/FetchDataSlice";
@@ -6,23 +6,45 @@ import { fetchdata } from "../../Slice/FetchDataSlice";
 export default function Cryptocurriencies() {
   const state = useSelector((state) => state.fetch);
   const Dispatch = useDispatch();
+  let [updatedarray, setUpdatedArray] = useState([]);
 
   if (state.data == null) {
     Dispatch(fetchdata());
   }
 
+  function FilterCurrencies(event) {
+    // console.log(event.target.value);
+    const updatedarray = state.data.data.coins.filter(
+      (coin) =>
+        coin.name.toLowerCase().includes(event.target.value.toLowerCase())
+      // console.log(coin.name.toLowerCase())
+    );
+
+    setUpdatedArray(updatedarray);
+    console.log(updatedarray);
+  }
+
   return (
     <div className="p-4 sm:ml-64 ">
       <div className="mt-12">
+        <input
+          type="text"
+          placeholder="Search..."
+          onChange={(e) => FilterCurrencies(e)}
+        />
         {state.isLoading == true ? (
           <div>
             <h1>Loading...</h1>
           </div>
         ) : (
           <div>
-            {state.data.data.coins.map((item) => {
-              return <CryptoCard item={item} key={item.rank}></CryptoCard>;
-            })}
+            {updatedarray.length != 0
+              ? updatedarray.map((item) => {
+                  return <CryptoCard item={item} key={item.rank}></CryptoCard>;
+                })
+              : state.data.data.coins.map((item) => {
+                  return <CryptoCard item={item} key={item.rank}></CryptoCard>;
+                })}
           </div>
         )}
       </div>
